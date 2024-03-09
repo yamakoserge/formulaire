7443088a50ca82a589595fa690b4e80842771661
Serges YAMAKO <yamakoserges@gmail.com>
2024-03-09 16:41:01 +0100

    contact form for starting collaboration

---
+++ src/app/pages/formulaire/formulaire.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire',
+  templateUrl: './formulaire.component.html',
+  styleUrl: './formulaire.component.css'
+})
+export class FormulaireComponent {
+
+}
